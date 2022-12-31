be4b47356100f27606ba69925c267089a3774e74
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-31 11:52:39 +0530

    Tic Tac Toe game board created

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+    static String[] board;
+    static void printBoard(){
+        System.out.println("|----|----|----|");
+        System.out.println("|" + board[0] + "|" + board[1] + "|" + board[2] + "|");
+        System.out.println("|--------------|");
+        System.out.println("|" + board[3] + "|" + board[4] + "|" + board[5] + "|");
+        System.out.println("|--------------|");
+        System.out.println("|" + board[6] + "|" + board[7] + "|" + board[8] + "|");
+        System.out.println("|----|----|----|");
+    }
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        board=new String[9];
         System.out.println("Welcome to Tic Tac Toe game");
+        printBoard();
     }
 }

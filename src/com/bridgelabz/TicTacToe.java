0414f51c0c7e32e8ba6f932acbf69c47de8d7e8b
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-01-05 20:40:18 +0530

    Added:Player to choose symbol X or O

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,7 +1,33 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+    static char[] board= new char[10];
+    static char playerSymbol;
+    static char computerSymbol;
+    static void createBoard(){
+        for(int i = 1;i < board.length;i++){
+            board[i] = ' ';
+        }
+        System.out.println("board is created");
+    }
+    static void assignSymbol(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Choose your letter X or O");
+        playerSymbol=scanner.next().charAt(0);
+        if(playerSymbol=='X'|| playerSymbol=='x'){
+            computerSymbol='O';
+        } else if (playerSymbol=='O' || playerSymbol=='o') {
+            computerSymbol='X';
+        }else {
+            System.out.println("Invalid input");
+            assignSymbol();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game");
+        createBoard();
+        assignSymbol();
     }
 }
